1eb4abea7a1077c25dd1be3fc515009d14dffc8e
julien <julienyang12@gmail.com>
2025-03-15 16:26:55 -0400

    CAN Structures for Payload messages

---
+++ main/Core/Inc/CAN_bus.h
@@ -0,0 +1,59 @@
+/**
+ * @file CAN_bus.h
+ * @author julien
+ * @date 2025-03-15
+ * @brief CAN bus message structures and unions. Unions are the same structures but available as an 64bit array.
+ */
+#ifndef __CAN_BUS_H
+#define __CAN_BUS_H
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+// CAN Message structures
+struct CAN_msg_1_s
+{
+    bool ok;
+    bool sampling_state;
+    bool temp_ctrl_state;
+    uint8_t target_temp;
+    uint16_t current_temp;
+    uint8_t battery_voltage; // Data type not specified on interfacing requirements, range of 10.0-25.0V required
+};
+
+struct CAN_msg_2_s
+{
+    uint16_t frqX;
+    uint16_t frqY;
+    uint16_t frqZ;
+    uint16_t ampX;
+};
+
+struct CAN_msg_3_s
+{
+    uint16_t ampY;
+    uint16_t ampZ;
+    uint32_t time_elapsed;
+};
+
+// Message unions
+union CAN_msg_1_u
+{
+    struct CAN_msg_1_s msg;
+    uint8_t bytes[8];
+};
+
+union CAN_msg_2_u
+{
+    struct CAN_msg_2_s msg;
+    uint8_t bytes[8];
+};
+
+union CAN_msg_3_u
+{
+    struct CAN_msg_3_s msg;
+    uint8_t bytes[8];
+};
+
+#endif // __CAN_BUS_H
