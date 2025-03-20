d40ed14c0dfd75e3ea6875ab38d751029f2a0483
Akash <akash.w.shah@gmail.com>
2025-03-20 19:25:40 -0400

    Blinky code in main for LD1, LD2

--- main/Core/Src/main.c
+++ main/Core/Src/main.c
@@ -121,6 +121,9 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+	  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
