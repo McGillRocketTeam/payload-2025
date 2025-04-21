35d06eb4b19dbbc582c9a05bb35a0368828319d0
Akash <akash.w.shah@gmail.com>
2025-04-21 17:36:25 -0400

    Move serial monitor code to separate header and source file

---
+++ main/Core/Src/serial_monitor.c
@@ -0,0 +1,24 @@
+/*
+ * serial_monitor.c
+ *
+ *  Created on: Apr 21, 2025
+ *      Author: akash
+ */
+
+#include "serial_monitor.h"
+#include "enabled.h"
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+#if SERIAL_MONITOR_ENABLED
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART1 and Loop until the end of transmission */
+  HAL_UART_Transmit(&huart4, (uint8_t *)&ch, 1, 0xFFFF);
+#endif
+  return ch;
+}
