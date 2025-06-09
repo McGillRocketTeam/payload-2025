99034a486742bada46c85acdac31707e76798450
Akash <akash.w.shah@gmail.com>
2025-06-09 16:55:25 -0400

    Auto disable serial monitor in final build Also alphabetize enable directives in enabled.h

--- main/Core/Inc/enabled.h
+++ main/Core/Inc/enabled.h
@@ -12,11 +12,23 @@
 // Set to 0 to disable a feature
 #define FINAL_BUILD 0
 
-#define CAN_BUS_ENABLED 1
-#define SD_CARD_ENABLED 1
-#define PELTIER_ENABLED 1
-#define SERIAL_MONITOR_ENABLED 1
-#define TEMPERATURE_SENSOR_ENABLED 1
+// Independent features
 #define BLINK_ENABLED 1
+#define CAN_BUS_ENABLED 1
+#define PELTIER_ENABLED 1
+#define SD_CARD_ENABLED 1
+#define TEMPERATURE_SENSOR_ENABLED 1
+
+// SERIAL_MONITOR_ENABLED
+#if FINAL_BUILD
+/*
+ * Automatically disable serial monitor in final build.
+ * DO NOT CHANGE THIS VALUE.
+*/
+#define SERIAL_MONITOR_ENABLED 0
+#else
+// Change this value
+#define SERIAL_MONITOR_ENABLED 1
+#endif
 
 #endif
