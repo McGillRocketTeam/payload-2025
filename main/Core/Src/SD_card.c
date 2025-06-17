41b410462020bed0d0cadce2ab7e28c5483efa92
Akash <akash.w.shah@gmail.com>
2025-06-17 18:14:05 -0400

    Check for SD card insertion on initialization

--- main/Core/Src/SD_card.c
+++ main/Core/Src/SD_card.c
@@ -6,12 +6,20 @@
  */
 
 #include <stdio.h>
+#include "stm32f446xx.h"
 #include "SD_card.h"
 #include "enabled.h"
 
 bool PL_SDCard_Init(PL_SDCard_Handler *sd_card, FATFS *fs, FIL *file)
 {
 #if SD_CARD_ENABLED
+    // Ensure SD card is inserted (the GPIO pin is low when the card is inserted)
+    if (HAL_GPIO_ReadPin(SD_DETECT_GPIO_Port, SD_DETECT_Pin))
+    {
+        // SD card is not inserted
+        return false;
+    }
+
     sd_card->fs = fs;
     sd_card->file = file;
 
