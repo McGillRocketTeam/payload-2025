646ad26cb7266c6a2bb2d8e202ee10891715c3ef
Akash <akash.w.shah@gmail.com>
2025-07-02 20:18:24 -0400

    Update temperature comment

--- main/Core/Src/temperature.c
+++ main/Core/Src/temperature.c
@@ -1,4 +1,7 @@
 #include "temperature.h"
 
-// Temperature values are in degrees Celsius. To be finalized by: payload software + ground station teams
+/**
+ * Temperature values are in degrees Celsius.
+ * The temperature indices are evaluated by the AV FC, so make sure AV has the same values.
+ */
 float temperatures[N_TEMPERATURES] = {1, 5, 10, 15, 20, 25, 30, 37};
