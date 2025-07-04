48a71ed6e7b52a3dbbd3f010f57b6346ae6d0455
Akash <akash.w.shah@gmail.com>
2025-07-03 21:36:37 -0400

    PL_Log function for pretty logging

--- main/Core/Inc/serial_monitor.h
+++ main/Core/Inc/serial_monitor.h
@@ -24,4 +24,60 @@ int printf(const char *restrict format, ...);
 extern UART_HandleTypeDef huart4;
 #define SERIAL_MONITOR_UART huart4
 
+// ANSI color escape codes
+#define COLOR_RESET          "\x1b[0m"
+#define COLOR_NONE           ""
+#define COLOR_RED            "\x1b[31m"
+#define COLOR_GREEN          "\x1b[32m"
+#define COLOR_YELLOW         "\x1b[33m"
+#define COLOR_BLUE           "\x1b[34m"
+#define COLOR_MAGENTA        "\x1b[35m"
+#define COLOR_CYAN           "\x1b[36m"
+#define COLOR_BRIGHT_RED     "\x1b[91m"
+#define COLOR_BRIGHT_GREEN   "\x1b[92m"
+#define COLOR_BRIGHT_YELLOW  "\x1b[93m"
+#define COLOR_BRIGHT_BLUE    "\x1b[94m"
+#define COLOR_BRIGHT_MAGENTA "\x1b[95m"
+#define COLOR_BRIGHT_CYAN    "\x1b[96m"
+
+// Log type colors
+#define COLOR_GENERAL            COLOR_NONE
+#define COLOR_ACCELEROMETER      COLOR_YELLOW
+#define COLOR_ADC                COLOR_BRIGHT_MAGENTA
+#define COLOR_BLINK              COLOR_BRIGHT_CYAN
+#define COLOR_CAN_BUS            COLOR_BLUE
+#define COLOR_PELTIER            COLOR_BRIGHT_BLUE
+#define COLOR_SD_CARD            COLOR_RED
+#define COLOR_TEMPERATURE_SENSOR COLOR_MAGENTA
+#define COLOR_DEBUG              COLOR_GREEN
+
+enum log_type 
+{
+   LOG_GENERAL,
+   LOG_ACCELEROMETER,
+   LOG_ADC,
+   LOG_BLINK,
+   LOG_CAN_BUS,
+   LOG_PELTIER,
+   LOG_SD_CARD,
+   LOG_TEMPERATURE_SENSOR,
+   LOG_DEBUG = -1
+};
+
+// Log status colors
+#define COLOR_INITIALIZING COLOR_MAGENTA
+#define COLOR_OK           COLOR_GREEN
+#define COLOR_WARNING      COLOR_YELLOW
+#define COLOR_ERROR        COLOR_RED
+
+enum log_status
+{
+   LOG_INITIALIZING,
+   LOG_OK,
+   LOG_WARNING,
+   LOG_ERROR
+};
+
+int PL_Log(const char *restrict format, enum log_type type, enum log_status status, ...);
+
 #endif /* INC_SERIAL_MONITOR_H_ */
--- main/Core/Src/serial_monitor.c
+++ main/Core/Src/serial_monitor.c
@@ -5,6 +5,8 @@
  *      Author: akash
  */
 
+#include <stdarg.h>
+#include <stdio.h>
 #include "serial_monitor.h"
 #include "enabled.h"
 
@@ -21,4 +23,104 @@ PUTCHAR_PROTOTYPE
   HAL_UART_Transmit(&SERIAL_MONITOR_UART, (uint8_t *)&ch, 1, 0xFFFF);
 #endif
   return ch;
+}
+
+int PL_Log(const char *restrict format, enum log_type type, enum log_status status, ...)
+{
+#if SERIAL_MONITOR_ENABLED
+  // Initialize variable arg list
+  va_list args;
+  // `va_start` requires the last non-variadic argument
+  va_start(args, status);
+
+  // Print logging time
+  if (printf("[%10ld] ", HAL_GetTick()) == EOF)
+  {
+    return EOF;
+  }
+
+  char *color;
+
+  // Print logging type
+  char *type_string;
+  switch (type)
+  {
+  case LOG_GENERAL:
+    type_string = "GENERAL";
+    color = COLOR_GENERAL; 
+    break;
+  case LOG_ACCELEROMETER:
+    type_string = "ACCELEROMETER";
+    color = COLOR_ACCELEROMETER;
+    break;
+  case LOG_ADC:
+    type_string = "ADC";
+    color = COLOR_ADC;
+    break;
+  case LOG_BLINK:
+    type_string = "BLINK";
+    color = COLOR_BLINK;
+    break;
+  case LOG_CAN_BUS:
+    type_string = "CAN BUS";
+    color = COLOR_CAN_BUS;
+    break;
+  case LOG_PELTIER:
+    type_string = "PELTIER";
+    color = COLOR_PELTIER;
+    break;
+  case LOG_SD_CARD:
+    type_string = "SD CARD";
+    color = COLOR_SD_CARD;
+    break;
+  case LOG_TEMPERATURE_SENSOR:
+    type_string = "TEMP SENSOR";
+    color = COLOR_TEMPERATURE_SENSOR;
+    break;
+  case LOG_DEBUG:
+    type_string = "DEBUG";
+    color = COLOR_DEBUG;
+    break;
+  }
+  if (printf("[%s%-15s%s] ", color, type_string, COLOR_RESET) == EOF)
+  {
+    return EOF;
+  }
+
+  // Print logging status
+  char *status_string;
+  switch (status)
+  {
+  case LOG_INITIALIZING:
+    status_string = "INIT";
+    color = COLOR_INITIALIZING;
+    break;
+  case LOG_OK: 
+    status_string = "OK";
+    color = COLOR_OK;
+    break;
+  case LOG_WARNING:
+    status_string = "WARNING";
+    color = COLOR_WARNING;
+    break;
+  case LOG_ERROR:
+    status_string = "ERROR";
+    color = COLOR_ERROR;
+    break;
+  }
+  if (printf("[%s%-5s%s] ", color, status_string, COLOR_RESET) == EOF)
+  {
+    return EOF;
+  }
+
+  // Write string formatted by va list into the string buffer
+  static char buffer[256];
+  vsnprintf(buffer, sizeof(buffer), format, args);
+
+  // Release `va_list`
+  va_end(args);
+
+  // Print formatted message and newline
+  return printf("%s\r\n", buffer);
+#endif
 }
